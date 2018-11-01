6607a83c5826cfa8d2c477a3be96a22ddfe2ebb7
johannesk00p <johanneskoop94@gmail.com>
2018-11-01 23:29:31 +0200

    j

---
+++ Labwork7/www/scripts/databaseHandler.js
@@ -0,0 +1,31 @@
+﻿var databaseHandler = {
+ db: null,
+ createDatabase: function () {
+ this.db = window.openDatabase(
+ "products.db",
+ "1.0",
+ "prodct database",
+ 1000000);
+ this.db.transaction(
+ function(tx) {
+ //Run SQL Here
+ tx.executeSql(
+ "create table if not exists product(_id int primary key, name
+text, quantity integer)",
+ [],
+ function (tx, results) { },
+ function (tx, error) {
+ console.log("Error while creating the table: " +
+error.message);
+ }
+ );
+ },
+ function (error) {
+ console.log("Transaction error:" + error.message);
+ },
+ function () {
+ console.log("Create DB transaction completed successfully:" );
+ },
+ );
+ }
+}
---
+++ Labwork7/www/scripts/productHandler.js
@@ -0,0 +1,19 @@
+﻿var productHandler = {
+    //Add the record in database, it adds record or row in Web SQL (SQLite)
+    addProduct: function (name, quantity) {
+        databaseHandler.db.transaction(
+            function (tx) {
+                tx.executeSql(
+                    "insert into product(name, quantity) values(?, ?)",
+                    [name, quantity],
+                    function (tx, results) { },
+                    function (tx, error) {
+                        console.log("add product error: " + error.message);
+                    }
+                );
+            },
+            function (error) { },
+            function () { }
+        );
+    },
+}
